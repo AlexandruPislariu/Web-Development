c0fc0dbdd6552616322a38e05299669d067892b9
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-07-24 10:39:12 +0300

    ajax approach to web dev

---
+++ AJAX/ajax.js
@@ -0,0 +1,13 @@
+/*
+  AJAX is an approach to web development
+  AJAX is formed of:
+    HTML
+    CSS
+    JavaScript
+    The DOM
+    XMLHTTP requests
+  Scope:
+    With AJAX, websites can send and request data from a server
+  in the background without disturbing the current page
+    => Single Page Apps
+*/
